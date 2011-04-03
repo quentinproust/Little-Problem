8b0e34bbda63fa0021282e292d373ac2e24b63b2
q.proust <q.proust@gmail.com>
2011-04-03 10:47:01 +0200

    Add FakeItEasy for test + Test on ForEach extension for IEnumerable

---
+++ LittleProblem/LittleProblem.CommonTest/ForEachExtensionTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using LittleProblem.Common.Colllections;
+using Moq;
+using NUnit.Framework;
+
+namespace LittleProblem.CommonTest
+{
+    [TestFixture]
+    public class ForEachExtensionTest
+    {
+
+        [Test]
+        public void DontExecuteActionWhenEmptyEnumerable()
+        {
+            int count = 0; // Used to check that the action was not called.
+            IEnumerable<int> empty = new List<int>();
+
+            var fakeAction = new Action<int>(x => count++);
+
+            empty.ForEach(fakeAction);
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ExecuteActionForEveryElementInEnumerable()
+        {
+            int count = 0; // Used to check that the action was not called.
+            IEnumerable<int> empty = new List<int> { 1, 2, 3, 4, 5 };
+
+            var fakeAction = new Action<int>(x => count++);
+
+            empty.ForEach(fakeAction);
+            Assert.That(count, Is.EqualTo(5));
+        }
+    }
+}
